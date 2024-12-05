dd8b6d8f3e50e1228c94762363e828d7056648b0
RichJsNomad <karaeeeani@gmail.com>
2024-12-05 22:35:15 +0200

    connect DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("the server is running on 3000 port");
+  console.log("yes");
 });
